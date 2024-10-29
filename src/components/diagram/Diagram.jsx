2955aa31b540aacc0254c718a00cd2c881852f01
Ivan Holm <vinokur4515@gmail.com>
2024-10-30 01:58:47 +0300

    полу костыль

--- src/components/diagram/Diagram.jsx
+++ src/components/diagram/Diagram.jsx
@@ -2,6 +2,12 @@ import DiagramBar from "./DiagramBar"
 
 import  './Diagram.css'
 const Diagram = (props) =>{
+
+ const valueMonth = props.dataSets.map(date =>
+    date.value 
+ )
+ const maxValue = Math.max(valueMonth)
+
     return <div className="diagram"> 
         {props.dataSets.map(dataSet =>
         <DiagramBar value={dataSet.vale}
